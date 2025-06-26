2c7d923d66483f907ca00b08eeb4471249720643
maria123-30 <m.rosa30@escola.pr.gov.br>
2025-06-26 08:39:36 -0300

    kjiyg

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 function criaCartao(categoria, pergunta, resposta) {
   let container = document.getElementById('container')
+  let cartao =document.createElement('article')
+  cartao.className = 'cartao'
+
+  cartao.innerHTML = `
+  <div class="cartao__conteudo">
+  <h3>geografia</h3>
+  <div class="cartao__conteudo__pergunta">
+      <p>Qual é a capital do Paraná?</p>
+  </div>
+  <div class="cartao__conteudo__resposta">
+      <p>A capital é Curitiba</p> 
+  </div>
+  </div>
+  `
+container.appendChild(cartao)
+
 }
